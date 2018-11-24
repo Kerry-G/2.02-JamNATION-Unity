66aaa2ca82f9389e579de68ce27f97a59f1be9da
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2018-11-24 13:19:34 -0500

    WIP: Laser hit detection

---
+++ Assets/_Scripts/Entities/MonoBehaviors/Player/Beam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(LineRenderer))]
+
+public class Beam : MonoBehaviour {
+
+	public float laserWidth = 1.0f;
+	public float laserLenght = 100f;
+	private LineRenderer _lineRenderer;
+	
+	void Start() {
+		_lineRenderer = GetComponent<LineRenderer>();
+		_lineRenderer.positionCount = 2; //number of vertices 
+		_lineRenderer.startWidth = laserWidth;
+		_lineRenderer.endWidth = laserWidth;
+	}
+	
+	private void Shoot() {
+		RaycastHit hit;
+		Vector3    start = transform.position + Vector3.up;
+
+		if ( Physics.Raycast(start, transform.forward, out hit, 100.0f) ) {
+			print("Found an object: " + hit.collider.gameObject + " - distance: " + hit.distance);
+			_lineRenderer.SetPosition(0, start);
+			_lineRenderer.SetPosition(1, hit.point);
+		} else {
+			_lineRenderer.SetPosition(0, start);
+			_lineRenderer.SetPosition(1, transform.forward * laserLenght);
+		}
+	}
+
+}
