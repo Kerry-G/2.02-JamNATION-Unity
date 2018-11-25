da4dc952be28536da29b8eca843a099d22d6874a
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2018-11-24 20:35:15 -0500

    JF integration

---
+++ Assets/_Scripts/UI/Menu_Buttons.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class Menu_Buttons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+
+	Coroutine hoverStart;
+	Coroutine hoverCredits;
+	Coroutine hoverExit;
+	Coroutine hoverCredOff;
+
+	private float boutonTemps = 1.5f;
+	float cursorTimer;
+
+	public GameObject creditsScreen;
+
+	public GameObject cursor;
+	Coroutine cursorScale;
+
+	private Vector3 maxSize;
+
+	void Start(){
+		maxSize = new Vector3(1, 1, 1);
+	}
+
+	public void OnPointerEnter(PointerEventData eventData){
+		if (gameObject.transform.tag == "Button") {
+			cursorScale = StartCoroutine (ScaleCursor ());
+		}
+		if (gameObject.transform.name == "Hover_Start") {
+			hoverStart = StartCoroutine (HoverStart ());
+		}
+		else if (gameObject.transform.name == "Hover_Credits") {
+			hoverCredits = StartCoroutine (HoverCredits ());
+		}
+		else if (gameObject.transform.name == "Hover_Exit") {
+			hoverExit = StartCoroutine (HoverExit ());
+		}
+		else if (gameObject.transform.name == "Back") {
+			hoverExit = StartCoroutine (HoverCredOff ());
+		}
+	}
+
+
+	public void OnPointerExit(PointerEventData eventData){
+		if (gameObject.transform.tag == "Button") {
+			StopCoroutine (cursorScale);
+			cursor.transform.localScale = new Vector3 (0, 0, 0);
+		}
+		if (gameObject.transform.name == "Hover_Start") {
+			StopCoroutine (hoverStart);
+		}
+		else if (gameObject.transform.name == "Hover_Credits") {
+			StopCoroutine (hoverCredits);
+		}
+		else if (gameObject.transform.name == "Hover_Exit") {
+			StopCoroutine (hoverExit);
+		}
+		else if (gameObject.transform.name == "Back") {
+			StopCoroutine (hoverCredOff);
+		}
+
+	}
+
+	IEnumerator ScaleCursor() {
+		cursorTimer = 0;
+		while (true) {
+			cursorTimer += Time.deltaTime;
+			cursor.transform.localScale = Vector3.Lerp (Vector3.zero, Vector3.one, cursorTimer / boutonTemps);
+			yield return new WaitForEndOfFrame ();
+		}
+	}
+
+	IEnumerator HoverStart() {
+		yield return new WaitForSeconds (boutonTemps);
+		StartGame ();
+		yield return null;
+	}
+	IEnumerator HoverCredits() {
+		yield return new WaitForSeconds (boutonTemps);
+		Credits ();
+		yield return null;
+	}
+	IEnumerator HoverExit() {
+		yield return new WaitForSeconds (boutonTemps);
+		Quit ();
+		yield return null;
+	}
+	IEnumerator HoverCredOff() {
+		yield return new WaitForSeconds (boutonTemps);
+		CredOff ();
+		yield return null;
+	}
+
+	public void StartGame () {
+		Debug.Log ("Start");
+//		SceneManager.LoadScene (1);
+	}
+
+	public void Credits () {
+		Debug.Log ("Credits");
+		creditsScreen.GetComponent<Animator> ().SetBool ("CreditsOn", true);
+	}
+
+	public void Quit () {
+		Debug.Log ("Quit");
+	//	Application.Quit ();
+	}
+
+	public void CredOff () {
+		Debug.Log ("CredOff");
+		creditsScreen.GetComponent<Animator> ().SetBool ("CreditsOn", false);
+	}
+
+		
+}
+
+
---
+++ Assets/_Scripts/UI/TextWin.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextWin : MonoBehaviour {
+
+
+	public string whoWins;
+	public int randomValue;
+
+	public int sharkWins;
+	public int lionWins;
+	public int cowWins;
+	public int bearWins;
+
+	public GameObject shark;
+	public GameObject lion;
+	public GameObject cow;
+	public GameObject bear;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Timer (1f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (whoWins == "Shark") {
+			gameObject.GetComponent<Text> ().text = "Shark Wins";
+			gameObject.GetComponent<Text> ().color = new Color32 (0x02, 0xE8, 0xFF, 0xFF);
+		} else if (whoWins == "Lion") {
+			gameObject.GetComponent<Text> ().text = "Lion Wins";
+			gameObject.GetComponent<Text> ().color = new Color32 (0xE9, 0x6E, 0x14, 0xFF);
+		} else if (whoWins == "Cow") {
+			gameObject.GetComponent<Text> ().text = "Cow Wins";
+			gameObject.GetComponent<Text> ().color = new Color32 (0x81, 0xE9, 0x1D, 0xFF);
+		} else if (whoWins == "Bear") {
+			gameObject.GetComponent<Text> ().text = "Bear Wins";
+			gameObject.GetComponent<Text> ().color = new Color32 (0xA5, 0x25, 0xD3, 0xFF);
+		} else if (whoWins == "Draw") {
+			gameObject.GetComponent<Text> ().text = "Draw";
+			gameObject.GetComponent<Text> ().color = new Color32 (0x32, 0x32, 0x32, 0xFF);
+		}
+
+		if (sharkWins > 8 || lionWins > 8 || cowWins > 8 || bearWins > 8) {
+			SceneManager.LoadScene (0);
+		}
+	}
+
+	IEnumerator Timer(float temps) {
+		randomValue = Random.Range (1, 6);
+		if (randomValue == 1) {
+			whoWins = "Shark";
+			sharkWins++;
+			for (int i = 0; i < sharkWins; i++) {
+				shark.transform.GetChild (i).gameObject.SetActive (true);
+			}
+		} else if (randomValue == 2) {
+			whoWins = "Lion";
+			lionWins++;
+			for (int i = 0; i < lionWins; i++) {
+				lion.transform.GetChild (i).gameObject.SetActive (true);
+			}
+		} else if (randomValue == 3) {
+			whoWins = "Cow";
+			cowWins++;
+			for (int i = 0; i < cowWins; i++) {
+				cow.transform.GetChild (i).gameObject.SetActive (true);
+			}
+		} else if (randomValue == 4) {
+			whoWins = "Bear";
+			bearWins++;
+			for (int i = 0; i < bearWins; i++) {
+				bear.transform.GetChild (i).gameObject.SetActive (true);
+			}
+		} else if (randomValue == 5) {
+			whoWins = "Draw";
+		}
+		yield return new WaitForSeconds (temps);
+		StartCoroutine (Timer (1f));
+		yield return null;
+	}
+}
